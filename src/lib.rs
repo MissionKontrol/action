ab187fa072d71839f1c77de8370150f5d4395220
Peter <sceptix@gmail.com>
2021-11-03 23:16:50 -0400

    works okay...well as written anyway

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
+use rand::Rng;
+
 pub type Action = Box<dyn ActionState>;
 pub type ACTION = dyn ActionState;
 
 impl ACTION {
-    pub fn new() -> Action {
-        let actor_list = vec!["actor_list".to_string()];
-        Box::new(ActionReady{ actor_list, readied_action: ActionType{}})
+    pub fn new(actor_list: ActorList) -> Action {
+        Box::new(ActionReady{ actor_list, readied_action: ActionType::Dodge})
     }
 }
 
 pub trait ActionState {
     fn get_state(&self) -> String;
-    fn get_actor_list(&self) -> Vec<String>;
+    fn get_actor_list(&self) -> ActorList;
     fn get_action(&self) -> String;
     fn execute_action(&self) -> Action;
 }
 
 #[derive(Clone)]
 pub struct ActionReady {
-    actor_list: Vec<String>,
+    actor_list: Vec<Actor>,
     readied_action: ActionType,
 }
 
@@ -26,15 +27,22 @@ impl ActionState for ActionReady {
         "Ready".to_string()
     }
 
-    fn get_actor_list(&self) -> Vec<String> {
+    fn get_actor_list(&self) -> ActorList {
         self.actor_list.clone()
     }
 
     fn get_action(&self) -> String {
-        "Tots prepared".to_string()
+        match &self.readied_action {
+            ActionType::Attack(description) => format!("get ATTACK: {}",description.target),
+            _ => "Other".to_string(),
+        }
     }
 
     fn execute_action(&self) -> Action {
+        if let ActionType::Attack(description) = &self.readied_action {
+            let result = description.execute(self.actor_list.clone());
+            return Box::new(ActionComplete{actor_list: result});
+        }
         Box::new(ActionComplete{actor_list: self.actor_list.clone()})
 
     }
@@ -42,14 +50,15 @@ impl ActionState for ActionReady {
 
 #[derive(Clone)]
 pub struct ActionComplete {
-    actor_list: Vec<String>,
+    actor_list: ActorList,
 }
 
 impl  ActionState for ActionComplete {
     fn get_state(&self) -> String {
         "Complete".to_string()
     }
-    fn get_actor_list(&self) -> Vec<String> {
+
+    fn get_actor_list(&self) -> ActorList {
         self.actor_list.clone()
     }
 
@@ -62,16 +71,95 @@ impl  ActionState for ActionComplete {
     }
 }
 
-#[derive(Clone, Copy)]
-struct ActionType {}
+#[derive(Clone)]
+pub enum ActionType {
+    Attack(Attack),
+    Dodge,
+    None,
+}
 
-struct Attack {
+#[derive(Debug, Clone)]
+pub struct Attack {
+    target: usize,
+    attack_die: u8,
+    damage_die: u8,
+}
 
+trait Actions {
+    fn execute(&self, current_actors: ActorList) -> ActorList;
+}
+
+impl Actions for Attack {
+    fn execute(&self, mut current_actors: ActorList) -> ActorList {
+        let result = Attack::roll_attack();
+        let mut target = current_actors.remove(self.target);
+        print!("Does this {} hit? ",&result);
+        if target.does_this_hit(result) {
+            println!("Yes!");
+            let damage = Attack::roll_damage(&self.damage_die);
+            target = target.take_damage(damage);
+        }
+
+            current_actors.push(target);
+            current_actors.to_owned()
+    }
 }
 
 impl Attack {
-    fn execute_attack(&self) {
-
+    fn roll_attack() -> u8 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(1..=20) as u8
     }
 
+    fn roll_damage(damage_die: &u8) -> u8 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(1..= *damage_die) as u8
+    }
+}
+
+type ActorList = Vec<Actor>;
+
+#[derive(Debug, Clone)]
+pub struct Actor {
+    id: usize,
+    team_id: u8,
+    armour_class: u8,
+    hit_points: u8,
+    hit_die: u8,
+    damage_die: u8,
+}
+
+impl Actor {
+    pub fn new(id: usize, team_id: u8, armour_class: u8, hit_points: u8, hit_die: u8, damage_die: u8) -> Self { 
+        Self { id, team_id, armour_class, hit_points, hit_die, damage_die } }
+
+    pub fn get_action(&self, actor_list: &ActorList) -> Action {
+        let action = self.decide_action(actor_list);
+        Box::new(ActionReady{ actor_list: actor_list.clone(), readied_action: action})
+    }
+
+    pub fn does_this_hit(&self, attack_roll: u8) -> bool {
+        if attack_roll as u8 > self.armour_class { return true }
+        return false
+    }
+
+    pub fn take_damage(mut self, damage: u8) -> Actor {
+        self.hit_points -= damage;
+        self
+    }
+
+    fn select_target(&self, actor_list:  &Vec<Actor>) -> Option<usize> {
+        let target_list = actor_list.iter().filter(|x| x.team_id != self.team_id).collect::<Vec<&Actor>>();
+        if target_list.len() > 0 { Some(target_list[0].id) }
+        else { None }
+    }
+
+    fn decide_action(&self, actor_list: &Vec<Actor>) -> ActionType {
+        if let Some(target) = self.select_target(actor_list) {
+            ActionType::Attack(Attack{target, attack_die: self.damage_die, damage_die: self.damage_die})
+        }
+        else {
+            ActionType::None
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use action::*;
 
 fn main() {
-    let action: Action = action::ACTION::new();
+    let player1 = Actor::new( 0,1,10, 20, 6, 6);
+    let player2= Actor::new( 1,2,10, 20, 6, 6);
+
+    let actor_list = vec![player1,player2];
+
+    let action = actor_list[0].get_action(&actor_list);
+
     println!("{}",action.get_action());
     println!("{}",action.get_state());
+    println!("{:?}",action.get_actor_list());
 
     let action: Action = action.execute_action();
     println!("{}",action.get_action());
     println!("{}",action.get_state());
+    println!("{:?}",action.get_actor_list());
+
+    let action = actor_list[1].get_action(&action.get_actor_list());
+
+    println!("{}",action.get_action());
+    println!("{}",action.get_state());
+    println!("{:?}",action.get_actor_list());
+
+    let action: Action = action.execute_action();
+    println!("{}",action.get_action());
+    println!("{}",action.get_state());
+    println!("{:?}",action.get_actor_list());
 }
